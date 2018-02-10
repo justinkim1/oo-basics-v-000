94223e95d1a2b9fc6f7675268888c851f839cea6
Justin K <justin-kim1@hotmail.com>
2018-02-10 21:51:48 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
