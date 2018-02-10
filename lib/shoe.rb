607bf199dede7dabc3ff369a506423a6096792dd
Justin K <justin-kim1@hotmail.com>
2018-02-10 21:52:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 class Shoe
-  attr_accessor :brand, :page_count, :genre
+  attr_accessor :brand, :color, :size,:material,:condition
   attr_reader :title
  
   def initialize(brand)
     @brand = brand
   end
  
-  def turn_page
+  def cobble
     puts "Flipping the page...wow, you read fast!"
   end
  
